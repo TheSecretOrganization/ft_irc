82935c7983e477177c142f9e16f94f1330d3a6ab
Neffi42 <114996380+Neffi42@users.noreply.github.com>
2024-06-18 20:49:15 +0200

    Implemented getter and setter for nickname

--- src/Client.cpp
+++ src/Client.cpp
@@ -4,7 +4,7 @@
 #include <iostream>
 #include <string>
 
-Client::Client(int fd) : socket(fd) {
+Client::Client(int fd) : socket(fd), name(""), nickname("") {
 	std::cout << "new client " << fd << std::endl;
 }
 
@@ -15,4 +15,10 @@ ClientSocket& Client::getSocket() { return socket; }
 void Client::sendMessage(std::string type, std::string message) const {
 	std::string packet = type + " " + message;
 	socket.sendPacket(packet);
+}
+
+std::string Client::getNickname() const { return nickname; }
+
+void Client::setNickname(const std::string& newNickname) {
+	nickname = newNickname;
 }
