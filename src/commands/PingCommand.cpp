79fe0385ada22ce3e0ecd2b157eb87ec490bf522
Neffi42 <114996380+Neffi42@users.noreply.github.com>
2024-06-19 16:41:03 +0200

    removed debug message in PING

--- src/commands/PingCommand.cpp
+++ src/commands/PingCommand.cpp
@@ -6,8 +6,6 @@ PingCommand::PingCommand() {}
 PingCommand::~PingCommand() {}
 
 void PingCommand::execute(Client* client, std::string args) {
-	std::cout << "Got ping from " << client->getSocket().getFd() << " with "
-			  << args << std::endl;
 	try {
 		client->sendMessage("PONG", args);
 	} catch (const std::exception& e) {
