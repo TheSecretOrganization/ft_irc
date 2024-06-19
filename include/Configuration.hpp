eec8d121833dcee66410b87fb7f73ee793ee88c1
Neffi42 <114996380+Neffi42@users.noreply.github.com>
2024-06-19 12:45:37 +0200

    Updated Configuration

--- include/Configuration.hpp
+++ include/Configuration.hpp
@@ -1,13 +1,35 @@
 #pragma once
 
 #include <cstddef>
+#include <string>
+
+#define USERLEN 12
+#define CREATION_DATE "06/07/2024"
+#define SERVER_NAME "webserv"
+#define NETWORK_NAME "CIRCé"
+#define VERSION "1.0"
+#define HOSTNAME "localhost"
 
 class Configuration {
-    private:
-        size_t userlen;
-    public:
-        Configuration();
-        ~Configuration();
+  private:
+	size_t userlen;
+	std::string creationDate;
+	std::string serverName;
+	std::string networkName;
+	std::string hostname;
+	std::string password;
+	std::string version;
 
-        size_t getUserlen() const;
+  public:
+	Configuration();
+	~Configuration();
+
+	size_t getUserlen() const;
+	const std::string& getCreationDate() const;
+	const std::string& getServerName() const;
+	const std::string& getNetworkName() const;
+	const std::string& getHostname() const;
+	const std::string& getPassword() const;
+	void setPassword(const std::string& newPassword);
+	const std::string& getVersion() const;
 };
--- src/Configuration.cpp
+++ src/Configuration.cpp
@@ -2,8 +2,29 @@
 
 #include <cstddef>
 
-Configuration::Configuration(): userlen(12) {}
+Configuration::Configuration()
+	: userlen(USERLEN), creationDate(CREATION_DATE), serverName(SERVER_NAME),
+	  networkName(NETWORK_NAME), hostname(HOSTNAME), password(""),
+	  version(VERSION) {}
 
 Configuration::~Configuration() {}
 
 size_t Configuration::getUserlen() const { return userlen; }
+
+const std::string& Configuration::getCreationDate() const {
+	return creationDate;
+}
+
+const std::string& Configuration::getServerName() const { return serverName; }
+
+const std::string& Configuration::getNetworkName() const { return networkName; }
+
+const std::string& Configuration::getHostname() const { return hostname; }
+
+const std::string& Configuration::getPassword() const { return password; }
+
+void Configuration::setPassword(const std::string& newPassword) {
+	password = newPassword;
+}
+
+const std::string& Configuration::getVersion() const { return version; }
