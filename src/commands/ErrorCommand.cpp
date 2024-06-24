41755da631837dbb0618e8332207b9747af29695
Neffi42 <114996380+Neffi42@users.noreply.github.com>
2024-06-24 13:49:26 +0200

    removed DEFINE ERROR

--- src/commands/ErrorCommand.cpp
+++ src/commands/ErrorCommand.cpp
@@ -4,17 +4,16 @@
 #include <exception>
 #include <iostream>
 
-#define ERROR "ERROR"
 #define DEFAULT_ERR_MSG "Unknown server error"
 
-ErrorCommand::ErrorCommand() : Command(ERROR, 1, 1) {}
+ErrorCommand::ErrorCommand() : Command("ERROR", 1, 1) {}
 
 ErrorCommand::~ErrorCommand() {}
 
 void ErrorCommand::execute(Client* client, std::string args) {
 	if (args.empty())
 		args = DEFAULT_ERR_MSG;
-	sendError(client, ERROR, args);
+	sendError(client, "ERROR", args);
 	try {
 		Server::getInstance().deleteClient(client);
 	} catch (const std::exception& e) {
