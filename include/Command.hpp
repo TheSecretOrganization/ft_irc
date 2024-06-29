5cf999088f014e9d26d1727464824c08149e8abe
Neffi <114996380+Neffi42@users.noreply.github.com>
2024-06-29 12:36:36 +0200

    moved static commands together

--- include/Command.hpp
+++ include/Command.hpp
@@ -21,12 +21,10 @@ class Command {
 	virtual ~Command();
 
 	virtual void execute(Client* client, std::string args) = 0;
-	static std::vector<std::string> split(const std::string& str, char del);
 
 	virtual bool alreadyRegistred(Client* client) const;
 	virtual bool passwdMismatch(Client* client,
 								const std::string& passWd) const;
-
 	virtual bool needMoreParams(Client* client,
 								const std::vector<std::string>& vecArgs) const;
 	virtual bool noSuchChannel(Client* client, Channel* channel,
@@ -35,6 +33,9 @@ class Command {
 	virtual bool userOnChannel(Client* client, Channel* channel,
 							   std::string nick) const;
 	virtual bool noSuchServer(Client* client, const std::string& server) const;
-
 	virtual bool chanOPrivsNeeded(Client* client, Channel* channel) const;
+
+	static std::vector<std::string> split(const std::string& str, char del);
+	static std::string trim(const std::string& str);
+	static std::string size_tToString(size_t value);
 };
